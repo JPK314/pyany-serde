5e156af5521383c9fb15c54a35e7c344dbea4fd0
Jonathan Keegan <jpk3.14@gmail.com>
2025-02-21 22:51:20 -0500

    remove remaining cdylib functionality

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    if std::env::var("CARGO_FEATURE_CDYLIB").is_ok() {
-        println!("cargo:rustc-crate-type=cdylib");
-    } else if std::env::var("CARGO_FEATURE_LIB").is_ok() {
-        println!("cargo:rustc-crate-type=lib");
-    }
-}
