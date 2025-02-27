2208cd2821a354aa90bb1c36ed35a94ce3efae1d
Jonathan Keegan <jpk3.14@gmail.com>
2025-02-27 04:24:44 -0500

    remove windows-based assumption on c types used to represent dtypes

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -3,5 +3,5 @@ mod numpy_dtype_enum;
 mod python_type;
 
 pub use align::get_bytes_to_alignment;
-pub use numpy_dtype_enum::{get_numpy_dtype, NumpyDtype};
+pub use numpy_dtype_enum::NumpyDtype;
 pub use python_type::{detect_python_type, get_python_type_byte, retrieve_python_type, PythonType};
--- src/common/numpy_dtype_enum.rs
+++ src/common/numpy_dtype_enum.rs
@@ -1,5 +1,5 @@
 use num_derive::{FromPrimitive, ToPrimitive};
-use numpy::{dtype, PyArrayDescr, PyArrayDescrMethods};
+use numpy::{dtype, Element, PyArrayDescr, PyArrayDescrMethods};
 use pyo3::{exceptions::PyValueError, intern, prelude::*};
 use strum_macros::{Display, EnumIter, EnumString};
 
@@ -52,43 +52,31 @@ impl<'py> FromPyObject<'py> for NumpyDtype {
             .getattr(intern!(py, "dtype"))?
             .call1((ob,))?;
         let dtype = dtype_any.downcast::<PyArrayDescr>()?;
-        match dtype.num() {
-            1 => Ok(NumpyDtype::INT8),
-            2 => Ok(NumpyDtype::UINT8),
-            3 => Ok(NumpyDtype::INT16),
-            4 => Ok(NumpyDtype::UINT16),
-            7 => Ok(NumpyDtype::INT32),
-            8 => Ok(NumpyDtype::UINT32),
-            9 => Ok(NumpyDtype::INT64),
-            10 => Ok(NumpyDtype::UINT64),
-            11 => Ok(NumpyDtype::FLOAT32),
-            12 => Ok(NumpyDtype::FLOAT64),
-            _ => Err(PyValueError::new_err(format!(
+        if dtype.is_equiv_to(&i8::get_dtype(py)) {
+            Ok(NumpyDtype::INT8)
+        } else if dtype.is_equiv_to(&u8::get_dtype(py)) {
+            Ok(NumpyDtype::UINT8)
+        } else if dtype.is_equiv_to(&i16::get_dtype(py)) {
+            Ok(NumpyDtype::INT16)
+        } else if dtype.is_equiv_to(&u16::get_dtype(py)) {
+            Ok(NumpyDtype::UINT16)
+        } else if dtype.is_equiv_to(&i32::get_dtype(py)) {
+            Ok(NumpyDtype::INT32)
+        } else if dtype.is_equiv_to(&u32::get_dtype(py)) {
+            Ok(NumpyDtype::UINT32)
+        } else if dtype.is_equiv_to(&i64::get_dtype(py)) {
+            Ok(NumpyDtype::INT64)
+        } else if dtype.is_equiv_to(&u64::get_dtype(py)) {
+            Ok(NumpyDtype::UINT64)
+        } else if dtype.is_equiv_to(&f32::get_dtype(py)) {
+            Ok(NumpyDtype::FLOAT32)
+        } else if dtype.is_equiv_to(&f64::get_dtype(py)) {
+            Ok(NumpyDtype::FLOAT64)
+        } else {
+            Err(PyValueError::new_err(format!(
                 "Invalid dtype: {}",
                 dtype.repr()?.to_str()?
-            ))),
+            )))
         }
     }
-}
-
-pub fn get_numpy_dtype(py_dtype: Py<PyArrayDescr>) -> PyResult<NumpyDtype> {
-    Python::with_gil(|py| {
-        let bound_dtype = py_dtype.into_bound(py);
-        match bound_dtype.num() {
-            1 => Ok(NumpyDtype::INT8),
-            2 => Ok(NumpyDtype::UINT8),
-            3 => Ok(NumpyDtype::INT16),
-            4 => Ok(NumpyDtype::UINT16),
-            7 => Ok(NumpyDtype::INT32),
-            8 => Ok(NumpyDtype::UINT32),
-            9 => Ok(NumpyDtype::INT64),
-            10 => Ok(NumpyDtype::UINT64),
-            11 => Ok(NumpyDtype::FLOAT32),
-            12 => Ok(NumpyDtype::FLOAT64),
-            _ => Err(PyValueError::new_err(format!(
-                "Invalid dtype: {}",
-                bound_dtype.repr()?.to_str()?
-            ))),
-        }
-    })
 }
