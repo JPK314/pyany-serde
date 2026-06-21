d39e804f75cd4ab148f02b7cf05ff771116a56ae
Jonathan Keegan <jpk3.14@gmail.com>
2026-06-21 17:30:26 -0400

    add more debug logs

--- src/tests/pickling/init_strategy.rs
+++ src/tests/pickling/init_strategy.rs
@@ -1,4 +1,4 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyDict};
 
 use crate::{pyany_serde_impl::InitStrategy, tests::run_python_test_file};
 
@@ -41,6 +41,52 @@ fn run_pickling_tests() -> PyResult<()> {
     println!("VIRTUAL_ENV: {}", std::env::var("VIRTUAL_ENV").unwrap());
     println!("PATH: {}", std::env::var("PATH").unwrap());
     Python::initialize();
+    Python::attach(|py| {
+        let os = py.import("os")?;
+        let environ = os.getattr("environ")?;
+        let environ = environ.cast::<PyDict>()?;
+
+        println!(
+            "PYTHONHOME={:?}",
+            environ
+                .get_item("PYTHONHOME")?
+                .map(|v| v.extract::<String>())
+                .transpose()?
+        );
+        println!(
+            "PYTHONPATH={:?}",
+            environ
+                .get_item("PYTHONPATH")?
+                .map(|v| v.extract::<String>())
+                .transpose()?
+        );
+
+        let sys = py.import("sys").unwrap();
+
+        println!(
+            "sys.executable = {:?}",
+            sys.getattr("executable")
+                .unwrap()
+                .extract::<String>()
+                .unwrap()
+        );
+        println!(
+            "sys.prefix = {:?}",
+            sys.getattr("prefix").unwrap().extract::<String>().unwrap()
+        );
+        println!(
+            "sys.base_prefix = {:?}",
+            sys.getattr("base_prefix")
+                .unwrap()
+                .extract::<String>()
+                .unwrap()
+        );
+        println!(
+            "sys.path = {:?}",
+            sys.getattr("path").unwrap().repr().unwrap()
+        );
+        Ok::<_, PyErr>(())
+    })?;
     Python::attach(|py| {
         run_python_test_file(
             py,
